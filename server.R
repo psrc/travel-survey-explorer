163a63d60b91fa17df4e20b02064cd17b977e0e0
Ennazus <childresssuzanne@gmail.com>
2024-10-18 15:51:12 -0700

    ui improvements

--- server.R
+++ server.R
@@ -1,77 +1,71 @@
-library(shiny)
-library(dplyr)
-library(gt)
-library(psrcplot)
-library(tidyr)
-
 server <- function(input, output, session) {
-
-# Reactive dataset
-dataset <- reactive({
-  req(input$travel, input$demographic, input$survey_year)  # Ensure inputs are available
   
-  # Filter the dataset based on user input
-  summary_tbl %>%
-    filter(travel_category == input$travel, 
-           demographic_category == input$demographic, 
-           survey_year == input$survey_year)
-})
-
-# Reactive dataset
-plot<- reactive({
-  dataset()
+  # Reactive dataset
+  dataset <- reactive({
+    req(input$travel, input$demographic, input$survey_year)  # Ensure inputs are available
+    
+    # Filter the dataset based on user input
+    summary_tbl %>%
+      filter(travel_category == input$travel, 
+             demographic_category == input$demographic, 
+             survey_year == input$survey_year)
+  })
   
-  interactive_column_chart(dataset(), x = 'travel_attribute', 
-                           y = 'prop', fill = 'demographic_attribute', moe='prop_moe')
-})
-
-# Render the filtered data as a gt table
-output$data <- render_gt({
-  dataset() %>%
-    select('travel_attribute', 'demographic_attribute', 'prop', 'prop_moe', 'est', 'count') %>%
-    rename('Topic of Interest' = 'travel_attribute') %>%
-    rename('Traveler Characteristic' = 'demographic_attribute') %>%
-    rename('Share' = 'prop') %>%
-    rename('Share Margin of Error' = 'prop_moe') %>%
-    rename('Sample Size'='count')%>%
-    rename('Total' = 'est') %>%
-    mutate('Total' = round(Total, -3)) %>%
-    gt() %>%
-    fmt_percent(columns = c('Share', 'Share Margin of Error'), decimals = 0) %>%
-    fmt_number(columns = 'Total', decimals = 0)
-})
-
-
-output$plot <- renderPlotly({
-  plot()
+  # Reactive plot
+  plot <- reactive({
+    dataset()
+    
+    interactive_column_chart(dataset(), x = 'travel_attribute', 
+                             y = 'prop', fill = 'demographic_attribute', moe='prop_moe')
+  })
   
-})
-
-# Download the filtered data as an Excel file
-output$downloadData <- downloadHandler(
-  filename = function() {
-    paste("Travel_Survey_Data_", Sys.Date(), ".xlsx", sep = "")
-  },
-  content = function(file) {
-    req(dataset())  # Ensure dataset is available
-    if (nrow(dataset()) == 0) {
-      stop("No data available for download.")  # Add error handling if dataset is empty
+  # Render the filtered data as a gt table
+  output$data <- render_gt({
+    dataset() %>%
+      select('travel_attribute', 'demographic_attribute', 'prop', 'prop_moe', 'est', 'count') %>%
+      rename('Topic of Interest' = 'travel_attribute') %>%
+      rename('Traveler Characteristic' = 'demographic_attribute') %>%
+      rename('Share' = 'prop') %>%
+      rename('Share Margin of Error' = 'prop_moe') %>%
+      rename('Sample Size' = 'count') %>%
+      rename('Total' = 'est') %>%
+      mutate('Total' = round(Total, -3)) %>%
+      gt() %>%
+      fmt_percent(columns = c('Share', 'Share Margin of Error'), decimals = 0) %>%
+      fmt_number(columns = 'Total', decimals = 0)
+  })
+  
+  # Render the plot
+  output$plot <- renderPlotly({
+    plot()
+  })
+  
+  # Download the filtered data as an Excel file
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste("Travel_Survey_Data_", Sys.Date(), ".xlsx", sep = "")
+    },
+    content = function(file) {
+      req(dataset())  # Ensure dataset is available
+      if (nrow(dataset()) == 0) {
+        stop("No data available for download.")  # Add error handling if dataset is empty
+      }
+      write_xlsx(dataset(), path = file)  # Write the dataset to an Excel file
     }
-    write_xlsx(dataset(), path = file)  # Write the dataset to an Excel file
-  }
-)
-
-# Download the plot as an HTML file
-output$downloadPlot <- downloadHandler(
-  filename = function() {
-    paste("Travel_Survey_Plot_", Sys.Date(), ".html", sep = "")
-  },
-  content = function(file) {
-    req(dataset())  # Ensure dataset is available
-    if (nrow(dataset()) == 0) {
-      stop("No data available for download.")  # Add error handling if dataset is empty
+  )
+  
+  # Download the plot as an HTML file
+  output$downloadPlot <- downloadHandler(
+    filename = function() {
+      paste("Travel_Survey_Plot_", Sys.Date(), ".html", sep = "")
+    },
+    content = function(file) {
+      req(dataset())  # Ensure dataset is available
+      if (nrow(dataset()) == 0) {
+        stop("No data available for download.")  # Add error handling if dataset is empty
+      }
+      saveWidget(as_widget(plot()), file)  # Save the plot as an HTML widget
     }
-    saveWidget(as_widget(plot()), file)  # Save the plot as an HTML widget
-  }
-)
+  )
 }
+
--- ui.R
+++ ui.R
@@ -6,18 +6,16 @@ ui <- navbarPage(
   windowTitle = "Travel Survey Explorer",
   
   # Custom CSS and Google Fonts for the navbar and general styling
-  # Link external CSS
   tags$head(
     tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
   ),
   
-  tabPanel("",
+  tabPanel("Dashboard",
            sidebarLayout(
              sidebarPanel(
                selectInput('survey_year', 'Survey Year', choices = unique(summary_tbl$survey_year), selected = 2023),
                selectInput('travel', 'Topic of Interest', choices = unique(summary_tbl$travel_category), selected = "Trip Mode"), 
                selectInput('demographic', 'Traveler Demographics', choices = unique(summary_tbl$demographic_category), selected = "Household Income"),
-        
                
                # Download buttons
                downloadButton("downloadData", "Download Table as Excel"),
@@ -39,9 +37,50 @@ ui <- navbarPage(
                )
              ),
              mainPanel(
-               plotlyOutput('plot'),
-               gt_output('data')
+               plotlyOutput('plot'),  # Interactive Plot
+               gt_output('data')  # Update this to gt_output for the gt table
+             )
+           )
+  ),
+  
+  # New FAQ tab
+  tabPanel("FAQ",
+           fluidPage(
+             h3("Frequently Asked Questions"),
+             p("Here you can find answers to commonly asked questions about the Travel Survey Explorer."),
+             
+             # Example FAQs
+             tags$ul(
+               tags$li(
+                 tags$b("What is the purpose of the Travel Survey Explorer?"),
+                 tags$p("The Travel Survey Explorer is designed to help analyze travel behavior data from Puget Sound Region Residents collected in the regional travel surveys.")
+               ),
+               tags$li(
+                 tags$b("Where can I download the full Household Travel Survey dataset, not just summary tables?"),
+                 tags$p(
+                   "The full set of household travel survey responses are available on the ", 
+                   tags$a(href = "https://household-travel-survey-psregcncl.hub.arcgis.com/", 
+                          "PSRC data portal", 
+                          target = "_blank"), "."
+                 )
+               ),
+               tags$li(
+                 tags$b("Where can I learn more about the Puget Sound Regional Council Travel Survey Program?"),
+                 tags$p(
+                   "You can learn more at the ", 
+                   tags$a(href = "https://www.psrc.org/our-work/household-travel-survey-program", 
+                          "PSRC Household Travel Survey Program page", 
+                          target = "_blank"), "."
+                 )
+               ),
+               tags$li(
+                 tags$b("Why are some variables such as disability status or sexuality not available in all years?"),
+                 tags$p("Questions have been added to the survey over time to accommodate communities and topics of interest to planning. In the case of disability status and sexuality, these questions were added in 2023.")
+               )
              )
            )
   )
 )
+
+
+
