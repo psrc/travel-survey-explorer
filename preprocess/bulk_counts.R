d02031c69dc129536b8c096c1e77c6b67fbcd195
jensenmj <mjensen@psrc.org>
2024-11-01 18:00:56 -0700

    Add additional variable combos--incl. person combos.

--- preprocess/bulk_counts.R
+++ preprocess/bulk_counts.R
@@ -49,7 +49,7 @@ demography      <- data.frame(
                  "worker",
                  "disability_person",
                  "hh_race_category",
-                 "vehicle_yn",
+                 "veh_yn",
                  "gender_bin3",
                  "sexuality_bin3",
                  "rent_own_bin2"),
@@ -67,24 +67,43 @@ demography      <- data.frame(
 ) %>% setDT()
 
 trip_combos <- expand.grid(c(geography$report_var, demography$report_var), trip_topics$report_var) %>%
+#  rbind(expand.grid("dest_purpose_bin4",c("mode_basic","mode_acc_basic"))) # mode_acc_basic not coded yet
+  rbind(data.frame(Var1="dest_purpose_bin4",Var2="mode_basic")) %>% 
   transpose() %>% lapply(c)
-
+  
 person_combos <- expand.grid(c(geography$report_var, demography$report_var), person_topics$report_var) %>%
   transpose() %>% lapply(c)
 
+trip_x_trip_combos <- expand.grid("dest_purpose_bin4", c("mode_basic","mode_acc_basic"))
+
+
 # Helper functions --------------------
 
-explorer_counts <- function(grpvars, analysis_unit){
+explorer_stats <- function(grpvars, analysis_unit=day, stat_var=NULL){
   pfx <- c("demographic_", "travel_")
   sfx <- c("category", "attribute")
+  dvar <- rbind(demography, geography)[report_var==(grpvars[[1]])]$label
+  tvar <- if(!rlang::is_empty(stat_var)){
+    if(stat_var=="num_trips_wtd"){
+     "Average daily trips per person"
+    }else if(stat_var=="vmt_wtd"){
+      "Average daily VMT per person"
+    }
+  }else{
+    rbind(trip_topics, person_topics)[report_var==(grpvars[[2]])]$label
+  }
+    
   hts_data2 <- copy(hts_data)
   if(any("disability_person" %in% grpvars)){  # -- disability is new in 2023
     hts_data2 %<>% lapply(FUN=function(x) dplyr::filter(x, survey_year==2023))                 
   }
-  rs <- psrc_hts_stat(hts_data2, analysis_unit=analysis_unit, group_vars=grpvars, incl_na=FALSE) %>% setDT()
+  rs <- psrc_hts_stat(hts_data2, 
+                      analysis_unit=analysis_unit, 
+                      group_vars=grpvars, 
+                      stat_var=stat_var,
+                      incl_na=FALSE) %>% setDT()
   rs %<>% setnames(grpvars, paste0(pfx, sfx[[2]])) %>%
-    .[, `:=`(demographic_category = rbind(..demography, ..geography)[report_var==(grpvars[[1]])]$label, 
-             travel_category = rbind(..trip_topics, ..person_topics)[report_var==(grpvars[[2]])]$label)] %>%
+    .[, `:=`(demographic_category = ..dvar, travel_category = ..tvar)] %>%
     setcolorder(c("survey_year", paste0(pfx[[1]], sfx), paste0(pfx[[2]], sfx)))
 }
 
@@ -103,34 +122,26 @@ hts_data %<>%
   hts_bin_mode() %>%
   hts_bin_telecommute_trichotomy()
 
-hts_data$hh %<>%
-  .[, home_in_seattle:=factor(fcase(home_jurisdiction=="Seattle", "Seattle",
-                                    !is.na(home_jurisdiction), NA_character_),
-                              levels=c("Seattle"))]
-
 hts_data$hh %<>% setDT() %>% .[, `:=`(
-  veh_yn=factor(
-    fcase(grepl("^0",     as.character(vehicle_count)), "No vehicle",
-          grepl("^[1-9]", as.character(vehicle_count)), "1+ vehicle"),
-    levels= c("No vehicle","1+ vehicle")))]
-labelled::var_label(hts_data$hh$vehicle_count_bin4) <- "Number of vehicles"
-labelled::var_label(hts_data$hh$veh_yn) <- "Presence or absence of own vehicle(s)"   
+  home_in_seattle=factor(
+    fcase(home_jurisdiction=="Seattle", "Seattle",
+          !is.na(home_jurisdiction), NA_character_),
+    levels=c("Seattle")),
+  hh_race_category=factor(
+    fcase(grepl("^Some [oO]ther", as.character(hh_race_category)), NA_character_,
+          !is.na(hh_race_category), as.character(hh_race_category))))]
+
+hts_data$person %<>% setDT() %>% .[,
+  race_category:=factor(
+    fcase(grepl("^Some [oO]ther", as.character(race_category)), NA_character_,
+          !is.na(race_category), as.character(race_category)))]
 
 
 # Generate summaries ------------------
-trip_summary <- lapply(trip_combos, explorer_counts, analysis_unit="trip")
-person_summary <- lapply(person_combos, explorer_counts, analysis_unit="person")
+trip_summary <- lapply(trip_combos, explorer_stats, analysis_unit="trip")
+person_summary <- lapply(person_combos, explorer_stats, analysis_unit="person")
 summary_labeled <- rbind(rbindlist(trip_summary), rbindlist(person_summary))
+#trip_rate_summary <- lapply(trip_topics$report_var, explorer_stats, stat_var="num_trips_wtd")
+#vmt_rate_summary <- lapply(trip_topics$report_var, explorer_stats, stat_var="vmt__wtd")
 
 saveRDS(summary_labeled, 'hts_tbl_4_shiny.rds')
-
-hts_data$hh %<>% setDT() %>% .[, `:=`(
-  veh_yn=factor(
-    fcase(grepl("^0",     as.character(vehicle_count)), "No vehicle",
-          grepl("^[1-9]", as.character(vehicle_count)), "1+ vehicle"),
-    levels= c("No vehicle","1+ vehicle")))]
-labelled::var_label(hts_data$hh$vehicle_count_bin4) <- "Number of vehicles"
-labelled::var_label(hts_data$hh$veh_yn) <- "Presence or absence of own vehicle(s)"   
-
-
-
